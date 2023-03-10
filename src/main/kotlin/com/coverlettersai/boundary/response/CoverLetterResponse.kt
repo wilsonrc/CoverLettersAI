c14daf72e3df2a0ffd0098d464eca07c47f2a89c
Wilson <wilsonrc16@gmail.com>
2023-03-09 23:01:10 -0400

    - create cover letters endpoint - return basic mock data

---
+++ src/main/kotlin/com/coverlettersai/boundary/response/CoverLetterResponse.kt
@@ -0,0 +1,8 @@
+package com.coverlettersai.boundary.response
+
+data class CoverLetterResponse(
+    val title : String,
+    val content: String,
+    val coverLetterSize: String,
+    val mood: String
+)
